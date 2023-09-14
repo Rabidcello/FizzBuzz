3a3a9b5206d527dea75045df7aa16dc9a06975b0
Rabidcello <kolivcao@gmail.com>
2023-09-14 11:03:50 -0400

    h

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Multiples {
+
+import java.lang.Math;
+public class Multiples {
+
+    public static void main(String[] args) {
+        double t = Math.floor((double) (1000-1) / 3);
+        double f = Math.floor((double) (1000-1) / 5);
+        double d = Math.floor((double) (1000-1) / 15);
+        System.out.println((int)(t + f - d));
+    }
 }
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int count = 0;
+        while (n > 0)
+        {
+            count++;
+            if (n%2 == 0)
+            {
+                n = n/2;
+            }
+            else
+            {
+                n = n-1;
+            }
+        }
+        System.out.println(count);
+    }
+
 }
